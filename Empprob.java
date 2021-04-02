2b5f63dc23186edb89e5902d7071c48bc8d7c0e8
Naveen <naveentruly86@gmail.com>
2021-04-02 16:49:38 +0530

    Added: Calculate Wages For Month

--- Empprob.java
+++ Empprob.java
@@ -3,14 +3,17 @@ public class Empprob{
   public static final int IS_FULL_TIME = 1;
   public static final int IS_PART_TIME = 2;
   public static final int ERPH = 20;
+  public static final int NOWD = 20;
 
 public static void main(String[] args){
 
     // Variables
      int emphrs = 0;
      int empwage = 0;
+     int totalempwage = 0;
 
     //Computation
+     for( int days = 1; days <= NOWD; days++){
      int empCheck = (int) Math.floor(Math.random() * 10) % 3;
      switch (empCheck){
         case IS_FULL_TIME:
@@ -23,8 +26,11 @@ public static void main(String[] args){
               emphrs = 0;
        }
      empwage = ERPH * emphrs;
+     totalempwage += empwage;
      System.out.println("Employee wage: " + empwage);
+   }
+     System.out.println("Total Employee Wage: " + totalempwage);
+
   }
+
 }
-
-
