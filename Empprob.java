2e69e5e95be98e6fca4e2823e534a20396b78900
Naveen <naveentruly86@gmail.com>
2021-04-02 16:29:53 +0530

    Added: Employee Wage using Switch Case

--- Empprob.java
+++ Empprob.java
@@ -1,24 +1,27 @@
 public class Empprob{
 
+  public static final int IS_FULL_TIME = 1;
+  public static final int IS_PART_TIME = 2;
+  public static final int ERPH = 20;
 
- public static void main(String[] args){
-    // Constants
-     int IS_FULL_TIME = 1;
-     int IS_PART_TIME = 2;
-     int ERPH = 20;
+public static void main(String[] args){
 
     // Variables
      int emphrs = 0;
      int empwage = 0;
 
     //Computation
-     double empCheck = Math.floor(Math.random() * 10) % 3;
-     if (empCheck == IS_FULL_TIME)
-         emphrs = 8;
-     else if (empCheck == IS_PART_TIME)
-        emphrs = 4;
-     else
-        emphrs = 0;
+     int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+     switch (empCheck){
+        case IS_FULL_TIME:
+              emphrs = 8;
+              break;
+       case  IS_PART_TIME:
+              emphrs = 4;
+              break;
+       default:
+              emphrs = 0;
+       }
      empwage = ERPH * emphrs;
      System.out.println("Employee wage: " + empwage);
   }
