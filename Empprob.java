65675a27da364b75839064aace72c69863596645
Naveen <naveentruly86@gmail.com>
2021-04-02 15:57:05 +0530

    Added: Add Part time Employee

--- Empprob.java
+++ Empprob.java
@@ -4,6 +4,7 @@ public class Empprob{
  public static void main(String[] args){
     // Constants
      int IS_FULL_TIME = 1;
+     int IS_PART_TIME = 2;
      int ERPH = 20;
 
     // Variables
@@ -11,9 +12,11 @@ public class Empprob{
      int empwage = 0;
 
     //Computation
-     double empCheck = Math.floor(Math.random() * 10) % 2;
+     double empCheck = Math.floor(Math.random() * 10) % 3;
      if (empCheck == IS_FULL_TIME)
          emphrs = 8;
+     else if (empCheck == IS_PART_TIME)
+        emphrs = 4;
      else
         emphrs = 0;
      empwage = ERPH * emphrs;
