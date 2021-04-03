525c28d1c95c146d810b0479630ce1ffdf2c224a
Naveen <naveentruly86@gmail.com>
2021-04-03 15:38:44 +0530

    Added: Multiple Companies

--- Empprob.java
+++ Empprob.java
@@ -2,11 +2,8 @@ public class Empprob{
 
   public static final int IS_FULL_TIME = 1;
   public static final int IS_PART_TIME = 2;
-  public static final int ERPH = 20;
-  public static final int NOWD = 20;
-  public static final int MHIM = 100;
 
-public static int computeempwage() {
+public static int computeempwage(String company, int ERPH, int NOWD, int MHIM) {
 
     // Variables
      int emphrs = 0;
@@ -31,10 +28,12 @@ public static int computeempwage() {
      System.out.println("Days#: " + totalworkingdays + " Employee Hours: " +emphrs);
    }
      int totalempwage = totalemphrs * ERPH;
-     System.out.println("Total Employee Wage: " + totalempwage);
+     System.out.println("Total Employee Wage for company: " +company+" is: "+totalempwage);
      return totalempwage;
   }
     public static void main(String[] args) {
-       computeempwage();
+       computeempwage("Reliance", 30, 26, 120);
+       computeempwage("Spencers", 25, 23, 110);
+       computeempwage("Dmart", 20, 20, 100);
  }
 }
